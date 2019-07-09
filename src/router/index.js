aaac309e2032dbe8f861641c15fadb95aada87d1
wht <229079894@qq.com>
2019-07-09 21:19:00 +0800

    项目构建和引入vue-router

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,13 @@
+// 导入需要的组件
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from '@/views/login'
+// import Login from '../views/login/index'
+// 注册
 Vue.use(VueRouter)
 const router = new VueRouter({
   routes: [
-
+    { name: 'login', path: '/login', component: Login }
   ]
 })
 
---
+++ src/views/login/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="login-container">
+        <!-- <el-card class="login-box">
+            <- <img src="../../assets/images/timg.jpg" alt=""> -
+        </el-card> -->
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang='less'>
+    .login-container{
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        left: 0;
+        top: 0;
+        // 在容器里全屏显示  背景图定位 / 背景图尺寸 必须连写 在
+        background: url(../../assets/images/1.jpg) no-repeat center /cover;
+        .login-box {
+            width: 400px;
+            height: 300px;
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%,-50%)
+            // img {
+            //     display: block;
+            //     width: 200px;
+            //     height: 200px;
+            // }
+        }
+    }
+</style>
