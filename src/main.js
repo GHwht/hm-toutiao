2d32a138f4557839cc72a56659a8a377b1077026
wht <229079894@qq.com>
2019-07-09 18:20:42 +0800

    引入vue-router

--- src/main.js
+++ src/main.js
@@ -4,8 +4,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 导入element-ui
+import ELEMENTUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 导入router
+// 1 默认索引 文件夹下 存在 index.js index.vue index.json 默认加载这些文件
+// import router from './router'
+// 2. 在使用vue-cli的时候 @ 符号在路径中作为前缀使用的时候 代表src目录
+import router from '@/router'
+
+Vue.use(ELEMENTUI)
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
